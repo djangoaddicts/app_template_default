c830ac4d54c2eb9243acab6bcd201f0a45ec74db
David Slusser <dbslusser@gmail.com>
2023-08-14 16:45:00 -0700

    adding templates

--- models.py
+++ models.py
@@ -2,3 +2,8 @@ from django.db import models
 from django.urls import reverse
 from auditlog.registry import auditlog
 from handyhelpers.models import HandyHelperBaseModel
+
+
+
+# register models with auditlog
+# auditlog.register(MyModel)
--- urls/gui.py
+++ urls/gui.py
@@ -10,10 +10,17 @@ urlpatterns = [
     path("default", gui.Index.as_view(), name="default"),
     path("home", gui.Index.as_view(), name="home"),
 
-    path("dashboard/", report.{{ app_name|title }}AnnualProgressView.as_view(), name="dashboard"),
+    # list views
+    # path("list_mymodels/", gui.ListMymodels.as_view(), name="list_mymodels"),
+    
+    # detail views
+    # path("detail_mymodel/<int:pk>", gui.DetailMymodel.as_view(), name="detail_mymodel"),
+
+    # report views
+    path("dashboard/", report.{{ app_name|title }}Dashboard.as_view(), name="dashboard"),
     path("annual_progress/", report.{{ app_name|title }}AnnualProgressView.as_view(), name="annual_progress"),
     path("annual_stats/", report.{{ app_name|title }}AnnualStatView.as_view(), name="annual_stats"),
-    path("annual_trends/", report.{{ app_name|title }}rAnnualTrendView.as_view(), name="annual_trends"),
+    path("annual_trends/", report.{{ app_name|title }}AnnualTrendView.as_view(), name="annual_trends"),
 
 ]
     
